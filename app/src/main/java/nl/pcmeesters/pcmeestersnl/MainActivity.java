1df2a209b96375d85d170bafb7321c8b53a700bb
Sascha <sworms@avans.nl>
2016-06-23 13:12:03 +0200

    button response

--- app/src/main/java/nl/pcmeesters/pcmeestersnl/MainActivity.java
+++ app/src/main/java/nl/pcmeesters/pcmeestersnl/MainActivity.java
@@ -8,13 +8,15 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main2);
     }
 
 
@@ -23,5 +25,12 @@ public class MainActivity extends AppCompatActivity {
     private void testUpload()
     {
         int i = 3   +3;
+    }
+
+    public void changeText(View view) {
+        TextView antwoordA = (TextView) findViewById(R.id.antwoord3A);
+        antwoordA.setText("Heeft u schade op uw apparaat?");
+        ImageButton ButtonA = (ImageButton) findViewById(R.id.imageButtonA);
+        ButtonA.setImageResource(R.drawable.computer_schade);
     }
 }
