43a99191d0b5cd59fb6777cf745aeca7f2fb054b
camiel0910 <camiel0910@hotmail.com>
2016-06-23 13:16:28 +0200

    push again

--- app/src/main/java/nl/pcmeesters/pcmeestersnl/MainActivity.java
+++ app/src/main/java/nl/pcmeesters/pcmeestersnl/MainActivity.java
@@ -1,8 +1,13 @@
 package nl.pcmeesters.pcmeestersnl;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -10,6 +15,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,11 +26,12 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+    public void laptopSelected(View view) {
 
+    }
 
-    private void testUpload()
-    {
-        int i = 3   +3;
+    public void desktopSelected(View view) {
+
     }
 
     public void changeText(View view) {
@@ -33,4 +40,19 @@ public class MainActivity extends AppCompatActivity {
         ImageButton ButtonA = (ImageButton) findViewById(R.id.imageButtonA);
         ButtonA.setImageResource(R.drawable.computer_schade);
     }
+
+    public void call(View view) {
+        Toast toast = Toast.makeText(getApplicationContext(),
+                "calling", Toast.LENGTH_SHORT);
+        toast.show();
+            Intent in=new Intent(Intent.ACTION_DIAL,Uri.parse("0639664374"));
+            try{
+                startActivity(in);
+            }
+
+            catch (android.content.ActivityNotFoundException ex){
+                Toast.makeText(getApplicationContext(),"yourActivity is not founded",Toast.LENGTH_SHORT).show();
+            }
+    }
+
 }
