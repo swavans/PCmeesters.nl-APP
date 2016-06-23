ffa6a0e7bea8e7e0ac9c182488a5061a7ed28090
Sascha <sworms@avans.nl>
2016-06-23 13:41:09 +0200

    Email fixed

--- app/src/main/java/nl/pcmeesters/pcmeestersnl/MainActivity.java
+++ app/src/main/java/nl/pcmeesters/pcmeestersnl/MainActivity.java
@@ -54,5 +54,19 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(getApplicationContext(),"yourActivity is not founded",Toast.LENGTH_SHORT).show();
             }
     }
+    public void mail(View view)
+    {
+        /* Create the Intent */
+        final Intent emailIntent = new Intent(android.content.Intent.ACTION_SEND);
+
+/* Fill it with Data */
+        emailIntent.setType("plain/text");
+        emailIntent.putExtra(android.content.Intent.EXTRA_EMAIL, new String[]{"info@pcmeesters.nl"});
+        emailIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, "Reparatie aanvraag");
+        emailIntent.putExtra(android.content.Intent.EXTRA_TEXT, "Beste PCmeester," +
+                "");
+
+        this.startActivity(Intent.createChooser(emailIntent, "Send mail..."));
+    }
 
 }
